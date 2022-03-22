2a9cfb4c030e320b94ef49c441901eee2b8113bb
ferchobeatmaker <ferchobeatmaker@gmail.com>
2022-03-21 22:31:15 -0500

    Implementar la version

--- root/app.py
+++ root/app.py
@@ -5,14 +5,9 @@ from productos import Producto
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_word():
-    return "<B>Hello World!!</B>"
-
 @app.route("/<producto>")
 def buscar_Producto_en_wallapop(producto):
     wallapopoKeyWord = producto
-    #    wallapopoKeyWord=request.args.get('keywords', 'nintendo')
     wallapopUrl = f"https://api.wallapop.com/api/v3/general/search?keywords={wallapopoKeyWord}%20&category_ids=12900&filters_source=seo_landing&longitude=-3.69196&latitude=40.41956&order_by=closest"
     r = requests.get(wallapopUrl)
     objetos_return_api = r.json().get("search_objects")
